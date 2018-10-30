99c4c6cc46b5701c1420389a314049bb6bb2c8b7
longjunhao <longjunhao_ext@smartisan.com>
2018-10-30 19:55:59 +0800

    swipeLeftView demo's second commit

--- app/src/main/java/com/example/testapp/swipelayoutdemo/DragViewLayout.java
+++ app/src/main/java/com/example/testapp/swipelayoutdemo/DragViewLayout.java
@@ -50,9 +50,12 @@ public class DragViewLayout extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 //        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //测量内容区域
         mContentView.measure(widthMeasureSpec, heightMeasureSpec);
+        //测量滑动显示区域
         int dragViewMeasureSpecWidth = MeasureSpec.makeMeasureSpec(mDragViewWidth, MeasureSpec.EXACTLY);
         mDragView.measure(dragViewMeasureSpecWidth, heightMeasureSpec);
+        //确定自己的尺寸
         int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
         int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
         setMeasuredDimension(measureWidth, measureHeight);
@@ -67,7 +70,9 @@ public class DragViewLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        //布局内容区域
         mContentView.layout(0, 0, mContentViewWidth, mHeight);
+        //布局滑动显示区域
         mDragView.layout(mContentViewWidth, 0, mContentViewWidth + mDragViewWidth, mHeight);
     }
 
@@ -75,27 +80,45 @@ public class DragViewLayout extends ViewGroup {
     public boolean onTouchEvent(MotionEvent event) {
 //        return super.onTouchEvent(event);
         mHelper.processTouchEvent(event);
+        //这里必须返回true，ViewDragHelper才能正常监听
         return true;
     }
 
     @Override
     public void computeScroll() {
         super.computeScroll();
+        // mHelper 已经在continueSettling() 中帮我们做了移动，重绘了view的位置（这种改变位置的方法layout方法是兼容的）
         if (mHelper.continueSettling(true)) {
             invalidate();
         }
     }
 
     private class DragHelperCallback extends ViewDragHelper.Callback {
+
+        /**
+         * touch down 的时候调用，是否分析该view的touch时间
+         *
+         * @param child     按在了那个view
+         * @param pointerId
+         * @return true：分析该view的touch事件，开始监听move up事件；false：不去分析，没有任何效果
+         */
         @Override
         public boolean tryCaptureView(@NonNull View child, int pointerId) {
-//            return false;
+            //这里表示要分析mContentView和mDragView
             return mContentView == child || mDragView == child;
         }
 
+        /**
+         * 当touch move的时候回调
+         *
+         * @param child ：tryCaptureView中的child
+         * @param left  ：左侧边距（期望值）
+         * @param dx    ：本次期望移动距离
+         * @return ： 确定要移动多少，返回后正式开始移动。
+         */
         @Override
         public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
-//            return super.clampViewPositionHorizontal(child, left, dx);
+            //定义真正的偏移量
             int offset = 0;
 
             if (child == mContentView) {
@@ -119,38 +142,63 @@ public class DragViewLayout extends ViewGroup {
             return offset;
         }
 
+        /**
+         * 当view的位置改变时调用，也属于touch move 的监听，但不参与移动，只是move事件的额外的监听
+         * 作用：带动另外一个view移动，其实也可以在 clampViewPositionHorizontal 中实现
+         *
+         * @param changedView
+         * @param left
+         * @param top
+         * @param dx
+         * @param dy
+         */
         @Override
         public void onViewPositionChanged(@NonNull View changedView, int left, int top, int dx, int dy) {
-//            super.onViewPositionChanged(changedView, left, top, dx, dy);
+            //当view位置改变，刷新界面
             invalidate();
+
+            //如果changedView是mContentView，则带懂mDragView；
             if (changedView == mContentView) {
+                //翻遍mDragView的位置
                 mDragView.layout(mContentViewWidth + left, 0, mContentViewWidth + left + mDragViewWidth, mHeight);
             } else if (changedView == mDragView) {
                 mContentView.layout(left - mContentViewWidth, 0, left, mHeight);
             }
         }
 
+        /**
+         * 松开手的回调
+         *
+         * @param releasedChild ： 松开了那个view
+         * @param xvel          ： 松开时x方向上的速率
+         * @param yvel          ： 松开时y方向上的速率
+         */
         @Override
         public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
-//            super.onViewReleased(releasedChild, xvel, yvel);
             int left = releasedChild.getLeft();
             if (releasedChild == mContentView) {
                 if (left >= -mDragViewWidth / 2) {
+                    //内部开启了 Scroller 的startScroll，进行数据模拟，不断调用的是 computeScroll
                     mHelper.smoothSlideViewTo(releasedChild, 0, 0);
+                    //关闭
                     isOpened = false;
                 } else {
                     mHelper.smoothSlideViewTo(releasedChild, -mDragViewWidth, 0);
+                    //打开
                     isOpened = true;
                 }
             } else if (releasedChild == mDragView) {
                 if (left >= mContentViewWidth - mDragViewWidth / 2) {
                     mHelper.smoothSlideViewTo(releasedChild, mContentViewWidth, 0);
+                    //关闭
                     isOpened = false;
                 } else {
                     mHelper.smoothSlideViewTo(releasedChild, mContentViewWidth - mDragViewWidth, 0);
+                    //打开
                     isOpened = true;
                 }
             }
+            // 注意：一定要实时刷新界面
             invalidate();
         }
     }
